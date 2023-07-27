fec089c4c2a07b0ed0db54c902ae16d29099daf7
Dash <dkshowarth@gmail.com>
2023-07-26 20:20:57 -0700

    link prompt functions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const svgTemplate = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                        ${logoInfo}
-                    </svg>`
+const {generateSVG, collectLogoInfo} = require('./lib/prompt.js');
+
+collectLogoInfo();
